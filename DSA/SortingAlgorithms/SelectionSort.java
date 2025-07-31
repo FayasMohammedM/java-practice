f9fd5b734e37301356bacb4390157bdcf25704a2
FayasMohammedM <fayasmohammedm.tech@gmail.com>
2025-07-31 18:01:15 +0530

    Selection Sort

---
+++ DSA/SortingAlgorithms/SelectionSort.java
@@ -0,0 +1,38 @@
+package DSA.SortingAlgorithms;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        
+    
+    int nums[]={6,5,2,8,9};
+    int size = nums.length;
+    int temp =0;
+    int minIndex =-1;
+
+    System.out.println("Before sorting");
+    for (int i : nums) {
+        System.out.print(i+" ");
+    }
+    System.out.println();
+    for (int i = 0; i < size-1; i++) {
+        minIndex =i;
+        for (int j = i+1; j < size; j++) {
+            if (nums[minIndex]>nums[j]) {
+                minIndex =j;
+            }
+        }
+        temp=nums[minIndex];
+        nums[minIndex]=nums[i];
+        nums[i]=temp;
+
+        for (int num : nums) {
+            System.out.print(num +" ");
+        }
+        System.out.println();
+    }
+    System.out.println("After Sorting");
+    for (int i : nums) {
+        System.out.print(i+" ");
+    }
+    }
+}
